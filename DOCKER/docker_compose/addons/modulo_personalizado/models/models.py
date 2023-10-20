0418a457b41b8d5edcbdc080e7c268535734ad45
Óscar <oscarrodero@hotmail.com>
2023-10-20 11:59:18 +0200

    Modulo personalizado funcionando

--- DOCKER/docker_compose/addons/modulo_personalizado/models/models.py
+++ DOCKER/docker_compose/addons/modulo_personalizado/models/models.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
-class modulo_personalizado(models.Model):
-    _name = 'modulo_personalizado.modulo_personalizado'
-    _description = 'modulo_personalizado.modulo_personalizado'
-    name = fields.Char()
 
 class oscar_rodero(models.Model):
     _name = 'modulo_personalizado.oscar_rodero'
     _description = 'modulo_personalizado.oscar_rodero'
     name = fields.Char()
+    date = fields.Date()
+    description = fields.Char()
