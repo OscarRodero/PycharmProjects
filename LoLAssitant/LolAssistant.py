8a49018b329e7f5beb6ff6a9c2506ee13d206b24
Óscar Rodero <oscarrodero@hotmail.com>
2024-02-05 18:50:08 +0100

    Proyecto finalizado.

--- LoLAssitant/LolAssistant.py
+++ LoLAssitant/LolAssistant.py
@@ -3,7 +3,8 @@ import time
 import speech_recognition as sr
 import pyttsx3
 from FuncAux import *
-
+import webbrowser, os, subprocess
+ruta_lol = "C:\\Riot Games\\League of Legends\\LeagueClient.exe"
 def main():
     hablar("Iniciado el asistente de League of Legends.")
     hablar('Puedes comenzar a hablar')
@@ -17,11 +18,29 @@ def main():
                     primera_palabra = palabras[0]
                     ultima_palabra = palabras[-1]
                     if primera_palabra:
-                        if primera_palabra == "iniciar" and ultima_palabra == "partida":                                                   
+                        if primera_palabra == "iniciar" and ultima_palabra == "partida":     
+                            hablar("Iniciando partida...")
+                            try:
+                                os.chdir(os.path.dirname(ruta_lol))
+                                subprocess.run([ruta_lol])
+                            except FileNotFoundError:
+                                print("No se pudo encontrar el ejecutable de League of Legends. Verifica la ruta.")
+                            except Exception as e:
+                                print(f"Error al intentar iniciar League of Legends: {e}")                                       
                             jugarPartida()
-                        #mi_hilo = threading.Thread(target=lanzar_cronometro(primera_palabra, ultima_palabra))
-                        #mi_hilo = threading.Thread(target=lambda: lanzar_cronometro(primera_palabra, ultima_palabra))
-                        #mi_hilo.start()
+                        elif primera_palabra == "buscar":
+                            if len(palabras) >= 3:
+                                segunda_palabra = palabras[1]
+                                tercera_palabra = palabras[-1]
+                                hablar("Buscando jugador...")
+                                webbrowser.open(f'https://www.op.gg/summoners/euw/{segunda_palabra}-{tercera_palabra}')
+                                print("Buscando jugador...")
+                            else:
+                                print("Necesitas especificar el nombre de invocador.")
+                        elif primera_palabra == "salir":
+                            hablar("Saliendo del asistente, hasta luego.")
+                            print("Saliendo del asistente, hasta luego.")
+                            break;
             else:
                 print("No se pudo escuchar nada.")
 
---
+++ LoLAssitant/README.txt
@@ -0,0 +1,18 @@
+Proyecto creado por Óscar Rodero Rodríguez-Peral
+
+Se deben instalar las librerías especificadas en el archivo requirements.txt
+
+Para usar el proyecto, hay que ejecutar LolAssistant.py, una vez hecho esto, sólo hay que hablarle al bot.
+
+Los comandos que admite son los siguientes:
+    -Iniciar Partida (Inicia una partida nueva, lanzando 2 cronómetros para el primer dragon y el primer nashor de la partida)
+    -[Nombre de la línea] + [Nombre de hechizo de invocador] (Inicializa un cronómetro para el hechizo de invocador de esa posición.)
+    -Asesinado + [Dragón/Nashor] (Inicializa un cronómetro para el tiempo de reaparición del dragon o el nashor)
+    -Buscar + [Nombre del jugador] + [ID] (Busca a un jugador en OP.GG por su nick e ID en EUW)
+    -Finalizar (finaliza la partida actual)
+    -Salir (Cierra el programa)
+
+Notas adiconales:
+    Cambia la ruta del directorio por defecto de League Of Legends a tu ruta actual para permitir que el programa lance el juego por ti,
+    esto ocurrirá al iniciar una nueva partida.
+    Eliminar la ruta o poner otra simplemente hará que esta función no se cumpla.
